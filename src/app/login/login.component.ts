b615c9bdaa41b779a52355900d839d579fb4db11
unknown <johnuruvath26@gmail.com>
2022-12-03 11:41:26 +0530

    eventbinding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,24 +11,67 @@ export class LoginComponent {
   acno=""
   psw=""
 
-  userDetails={
+  userDetails:any={
     1000:{acno:1000,username:"anu",password:123,balance:0},
     1001:{acno:1001,username:"amal",password:123,balance:0},
     1002:{acno:1002,username:"arun",password:123,balance:0},
     1003:{acno:1003,username:"mega",password:123,balance:0}
   }
 
-  login(){
-    alert("login clicked")
+  // login(){
+  //   var acno=this.acno
+  //   var psw=this.psw
+  //   var userDetails=this.userDetails
+  //  // alert("login clicked")
+
+  //   if(acno in userDetails){
+  //     if(psw==userDetails[acno]['password']){
+  //       alert('login success')
+  //     }
+  //     else{
+  //       alert("incorrect password")
+  //     }
+  //   }
+  //   else{
+  //     alert("incorrect username")
+  //   }
+
+  
+  // }
+  login(a:any,b:any){
+
+    this.acno=a.value
+    this.psw=b.value
+
+
+    var acno=this.acno
+    var psw=this.psw
+    var userDetails=this.userDetails
+   
+
+    if(acno in userDetails){
+      if(psw==userDetails[acno]['password']){
+        alert('login success')
+      }
+      else{
+        alert("incorrect password")
+      }
+    }
+    else{
+      alert("incorrect username")
+    }
+
+  
   }
-  acnoChange(event:any){
+
+//   acnoChange(event:any){
           
-    this.acno=event.target.value
-  }
-  pswChange(event:any){
-    this.psw=event.target.value
+//     this.acno=event.target.value
+//   }
+//   pswChange(event:any){
+//     this.psw=event.target.value
     
     
-  }
+//   }
 
 }
