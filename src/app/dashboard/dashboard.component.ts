f18790f793b1bf9b49a7cee977e232c3cf71e4f4
unknown <johnuruvath26@gmail.com>
2022-12-14 21:27:50 +0530

    parentchild

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -8,7 +9,7 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  
+
 
   // acno=""
   // psw=""
@@ -17,35 +18,47 @@ export class DashboardComponent {
   // acno1=""
   // psw1=""
   // amnt1=""
+  dateandtime:any
+  acno:any
+  user = ""
+  constructor(private ds: DataService, private fb: FormBuilder, private router: Router) {
+    
+    this.dateandtime=new Date()
 
-  user=""
-  constructor(private ds:DataService,private fb:FormBuilder){
-  
+    
     //access username
-    this.user=this.ds.currentuser
+    this.user = this.ds.currentuser
   }
 
-  depositForm=this.fb.group({acno:['',[Validators.required,Validators.pattern('[0-9]+')]],psw:['',[Validators.required,Validators.pattern('[0-9]+')]],amnt:['',[Validators.required,Validators.pattern('[0-9]+')]]})
-  withdrawForm=this.fb.group({acno1:['',[Validators.required,Validators.pattern('[0-9]+')]],psw1:['',[Validators.required,Validators.pattern('[0-9]+')]],amnt1:['',[Validators.required,Validators.pattern('[0-9]+')]]})
+  depositForm = this.fb.group({ acno: ['', [Validators.required, Validators.pattern('[0-9]+')]], psw: ['', [Validators.required, Validators.pattern('[0-9]+')]], amnt: ['', [Validators.required, Validators.pattern('[0-9]+')]] })
+  withdrawForm = this.fb.group({ acno1: ['', [Validators.required, Validators.pattern('[0-9]+')]], psw1: ['', [Validators.required, Validators.pattern('[0-9]+')]], amnt1: ['', [Validators.required, Validators.pattern('[0-9]+')]] })
+  
+  
+  ngOnInit():void{
+    if(!localStorage.getItem("currentacno")){
+      alert("please login first")
+      this.router.navigateByUrl('')
+    }
 
+  }
 
   deposit() {
     var acno = this.depositForm.value.acno
     var psw = this.depositForm.value.psw
     var amnt = this.depositForm.value.amnt
 
-    if(this.depositForm.valid){
+    if (this.depositForm.valid) {
 
-    const result = this.ds.deposit(acno, psw, amnt)
+      const result = this.ds.deposit(acno, psw, amnt)
 
-    if (result) {
-      alert(`${amnt} credited to your ac and the balance is ${result}`)
+      if (result) {
+        alert(`${amnt} credited to your ac and the balance is ${result}`)
+      } else {
+        alert('incorrect acno or password')
+      }
     } else {
-      alert('incorrect acno or password')
+      alert("invalid form")
     }
-  }else{
-    alert("invalid form")
-  }
   }
 
   withdraw() {
@@ -53,16 +66,26 @@ export class DashboardComponent {
     var psw1 = this.withdrawForm.value.psw1
     var amnt1 = this.withdrawForm.value.amnt1
 
-    if(this.withdrawForm.valid){
+    if (this.withdrawForm.valid) {
 
 
-    const result = this.ds.withdraw(acno1, psw1, amnt1)
+      const result = this.ds.withdraw(acno1, psw1, amnt1)
 
-    if (result) {
-      alert(`${amnt1} debited from your ac and the balance is ${result}`)
+      if (result) {
+        alert(`${amnt1} debited from your ac and the balance is ${result}`)
+      }
+    } else {
+      alert("invalid form")
     }
-  }else{
-    alert("invalid form")
   }
+  logout() {
+    localStorage.removeItem("currentuser")
+    localStorage.removeItem("currentacno")
+    this.router.navigateByUrl('')
+
   }
+  delteconfirm(){
+    this.acno=JSON.parse(localStorage.getItem('currentacno') || '')
+  }
+
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -4,29 +4,57 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
+  userDetails:any
+  currentuser = ""
+  currentacno = ""
 
-  currentuser=""
-  currentacno=""
+  constructor() {
+    this.getdetails() 
+   }
 
-  constructor() { }
-
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:123,balance:0,transaction:[]},
-    1001:{acno:1001,username:"amal",password:123,balance:0,transaction:[]},
-    1002:{acno:1002,username:"arun",password:123,balance:0,transaction:[]},
-    1003:{acno:1003,username:"mega",password:123,balance:0,transaction:[]}
+  savedetails() {
+    if (this.userDetails) {
+      localStorage.setItem("database", JSON.stringify(this.userDetails))
+    }
+    if (this.currentuser) {
+      localStorage.setItem("currentuser", JSON.stringify(this.currentuser))
+    }
+    if (this.currentacno) {
+      localStorage.setItem("currentacno", JSON.stringify(this.currentacno))
+    }
   }
-  
-  
+
+
+  getdetails() {
+    if (localStorage.getItem("database")) {
+      this.userDetails = JSON.parse(localStorage.getItem("database") || "")
+    }
+    if (localStorage.getItem("currentuser")) {
+      this.currentuser = JSON.parse(localStorage.getItem("currentuser") || "")
+    }
+    if (localStorage.getItem("currentacno")) {
+      this.currentacno = JSON.parse(localStorage.getItem("currentacno") || "")
+    }
+  }
+
+
+  // userDetails: any = {
+  //   1000: { acno: 1000, username: "anu", password: 123, balance: 0, transaction: [] },
+  //   1001: { acno: 1001, username: "amal", password: 123, balance: 0, transaction: [] },
+  //   1002: { acno: 1002, username: "arun", password: 123, balance: 0, transaction: [] },
+  //   1003: { acno: 1003, username: "mega", password: 123, balance: 0, transaction: [] }
+  // }
+
+
   register(acno: any, uname: any, psw: any) {
     var userDetails = this.userDetails
     if (acno in userDetails) {
       return false
     }
     else {
-      userDetails[acno] = { acno, username: uname, password: psw, balance: 0 ,transaction:[]}
+      userDetails[acno] = { acno, username: uname, password: psw, balance: 0, transaction: [] }
       console.log(userDetails);
-
+      this.savedetails()
       return true
     }
   }
@@ -37,15 +65,14 @@ export class DataService {
 
     var userDetails = this.userDetails
 
-
-
     if (acno in userDetails) {
       if (psw == userDetails[acno]['password']) {
 
         //acnumber
-        this.currentacno=acno                         //or =userDetails[acno]["acno"]
+        this.currentacno = acno                         //or =userDetails[acno]["acno"]
         //store username
-        this.currentuser=userDetails[acno]["username"]
+        this.currentuser = userDetails[acno]["username"]
+        this.savedetails()
         return true
       }
       else {
@@ -64,7 +91,8 @@ export class DataService {
     if (acno in userDetails) {
       if (password == userDetails[acno]["password"]) {
         userDetails[acno]["balance"] += amnt
-        userDetails[acno]["transaction"].push({type:"CREDIT",amount:amnt})
+        userDetails[acno]["transaction"].push({ type: "CREDIT", amount: amnt })
+        this.savedetails()
         return userDetails[acno]["balance"]
       }
       else {
@@ -85,7 +113,8 @@ export class DataService {
       if (password == userDetails[acno]["password"]) {
         if (amnt <= userDetails[acno]["balance"]) {
           userDetails[acno]["balance"] -= amnt
-          userDetails[acno]["transaction"].push({type:"DEBIT",amount:amnt})
+          userDetails[acno]["transaction"].push({ type: "DEBIT", amount: amnt })
+          this.savedetails()
 
           return userDetails[acno]["balance"]
         }
@@ -106,7 +135,7 @@ export class DataService {
     }
   }
 
-  gettransaction(acno:any){
+  gettransaction(acno: any) {
     return this.userDetails[acno]["transaction"]
   }
 }
